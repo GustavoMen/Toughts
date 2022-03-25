66f81ca4872157fa7522068d2d5d7f336d14f2b1
Gustavo Mendonça <contato.gustavomendonca@gmail.com>
2022-03-24 23:04:48 -0300

    Criando Models/Fazendo css

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const app = express()
 
 const conn = require('./db/conn')
 
+// Models
+const Tought = require('./models/Tought')
+const User = require('./models/User')
+
+// Import Routes
+const toughtsRoutes = require('./routes/toughtsRoutes')
+
+// Import Controller
+const ToughtController = require('./controllers/ToughtController')
+
 //template engine
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
@@ -59,6 +69,11 @@ app.use((req, res, next) => {
 
     next()
 })
+
+// Routes // 
+app.use('/toughts', toughtsRoutes)
+
+app.get('/', ToughtController.showToughts)
 
 
 conn
